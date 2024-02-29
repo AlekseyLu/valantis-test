ac9b3324dbb64729ec8101532ecd630ed92bb49c
AlekseyLu <luzyanin-ae@yandex.ru>
2024-02-29 10:58:41 +0300

    fix: Исправил прод сборку

--- src/api/Endpoints.ts
+++
@@ -1,3 +0,0 @@
-export const Endpoints = {
-  get_ids: "",
-};
--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 import { md5 } from "js-md5";
+
 import useGetDate from "../hooks/useGetDate";
 
 export const axiosInstance = axios.create({});
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 axiosInstance.interceptors.request.use(async (config: any) => {
   const currentDate = useGetDate();
   const passStamp = "Valantis_" + currentDate;
